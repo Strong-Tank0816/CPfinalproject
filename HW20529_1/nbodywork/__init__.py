d69b2d40e60a68e084c86a0b9730fc9786ae1d61
Strong-Tank0816 <161279608+Strong-Tank0816@users.noreply.github.com>
2024-05-29 21:56:24 +0800

    Delete HW20529_1 directory

--- HW20529_1/nbodywork/__init__.py
+++
@@ -1,3 +0,0 @@
-from nbodywork.particles import Particles
-from nbodywork.simulator import NBodySimulator
-from nbodywork.visualization import load_files, save_movie, save_movie_outer, save_movie_inner
