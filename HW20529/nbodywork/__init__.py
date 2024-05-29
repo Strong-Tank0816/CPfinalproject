ab2cc0c83b1faa10f13b6ba3f6aca221ca066769
Strong-Tank0816 <161279608+Strong-Tank0816@users.noreply.github.com>
2024-05-29 16:13:59 +0800

    0529 update

---
+++ HW20529/nbodywork/__init__.py
@@ -0,0 +1,3 @@
+from nbodywork.particles import Particles
+from nbodywork.simulator import NBodySimulator
+from nbodywork.visualization import load_files, save_movie, save_movie_outer, save_movie_inner
